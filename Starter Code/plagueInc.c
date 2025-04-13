c166602ab9ed344ae8f58f05b3fb43f9257bd67c
ChrisBleck <cbleck0808@gmail.com>
2025-04-13 11:05:24 -0400

    Add people events and initializations

--- Starter Code/plagueInc.c
+++ Starter Code/plagueInc.c
@@ -1,33 +1,38 @@
 #include "plagueInc.h"
 
-
 void
 person_init(person * s, tw_lp * lp)
 {
   int i;
   tw_event *e;
   Msg_Data *m;
-
   s->moves_left = 30;
-  s->x_spot = rand(GRID_WIDTH);
-  s->y_spot = rand(GRID_HEIGHT);
-  s->infected_time = 0;
-  s->immune_time = 0;
-  s->susceptible = true;
+  s->x_spot = tw_rand_integer(lp->rng, 0, GRID_WIDTH-1);
+  s->y_spot = tw_rand_integer(lp->rng, 0, GRID_HEIGHT-1);
   s->alive = true;
-  s->previous_move = NULL;
-  s->next_move = assign_move()
 
-  e = tw_event_new(lp->gid, 0, lp); //0 is the offset - no clue what that is
-  m = tw_event_data(e); //tried looking at the documentation, wasn't sure, just followed from the examples
-  m->type = MOVE;
-  tw_event_send(e);
+  double r = tw_rand_unif(lp->rng);
+  if (r < TRANSMISSION_COEFFICIENT) {
+	s->infected_time = tw_now(lp);
+	s->susceptible = false;
+	s->infected = true;
+  } else{
+	s->susceptible = true;
+	s->infected = false;
+  }
+
+
+  e = tw_event_new(lp->gid, tw_rand_exponential(lp->rng, AVERAGE_MOVE_TIME), lp); //Schedules event at some future point in time
+  m = tw_event_data(e); //retrieves the data pointer for the message associated with scheduled event, can modify message from here
+  m->type = STATUS_CHECK;
+  m->infected_count = 0;
+  tw_event_send(e); //schedules event in ROSS
 }
 
 
 
-abs_directions assign_move(tw_lp * lp, int x_spot, int y_spot) {
-    abs_directions possible_moves[8]; 
+enum abs_directions assign_move(tw_lp * lp, int x_spot, int y_spot) {
+    enum abs_directions possible_moves[8]; 
     int count = 0;
 
     if (y_spot < GRID_HEIGHT - 1) {
@@ -57,6 +62,131 @@ abs_directions assign_move(tw_lp * lp, int x_spot, int y_spot) {
 
 
 
+tw_lpid get_location_id(int x_spot, int y_spot){
+
+}
+
+void
+person_event(person * s, tw_bf * bf, Msg_Data * msg, tw_lp * lp) {
+	//WHEN NOT SICK & ALIVE
+	bool new_infection;
+	bool new_recovery;
+	tw_stime now_time = tw_now(lp);
+	int x_spot = s->x_spot;
+	int y_spot = s->y_spot;
+	bool previously_infected = s->infected;
+	tw_event *e;
+	Msg_Data *m;
+	if (s->immune){
+		{
+			if(now_time - s->immune_start > IMMUNE_TIME) {
+				s->immune = false;
+				s->susceptible = true;
+			}
+		}
+	}
+
+	if (s->susceptible){
+		for (i = 0; i < msg->infected_count; i++){
+			if(tw_rand_unif(lp->rng) < TRANSMISSION_COEFFICIENT) {
+				s->susceptible = false;
+				s->infected_time = now_time;
+				s->infected = true;
+				new_infection = true;
+			}
+		}
+	} 
+	//WHEN SICK & ALIVE - DO WE EVEN WANT DEAD PEOPLE TO MOVE
+
+	else
+	{
+		if(tw_rand_unif(lp->rng) < LETHALITY) {
+			s->alive = false;
+			tw_lpid location_id = get_location_id(x_spot, y_spot);
+			e = tw_event_new(location_id, 1, lp);
+			m = tw_event_data(e); 
+		    m->type = DEPARTURE; //TREAT A DEATH AS A DEPARTURE FROM LOCATION AND ARRIVAL NOWHERE
+			tw_event_send(e); 
+		}
+		if(now_time - s->infected_time > INFECTION_TIME){
+			s->infected = false;
+			s->immune = true;
+			s->immune_start = now_time;
+			new_recovery = true;
+		}
+	}
+
+	//LET THEM MOVE AND SEND MESSAGES TO THEIR LOCATION LPs
+
+	if (s->alive) {
+		tw_lpid prev_location_id = get_location_id(x_spot, y_spot);
+		enum abs_directions next_move = assign_move(lp, x_spot, y_spot);
+		switch (next_move)
+		{
+		case NORTH: 
+			y_spot++;
+			break;
+		case NORTH_EAST:
+			y_spot++
+			x_spot++;
+		case EAST:
+			x_spot++;
+		case SOUTH_EAST:
+			y_spot--;
+			x_spot++;
+		case SOUTH:
+			y_spot--;
+		case SOUTH_WEST:
+			y_spot--;
+			x_spot--;
+		case WEST:
+			x_spot--;
+		case NORTH_WEST:
+			y_spot++;
+			x_spot--;
+
+		case NO_MOVE:
+			if(new_infection){
+				e = tw_event_new(prev_location_id, 1, lp);
+				m = tw_event_data(e); 
+				m->type = NEW_INFECTION //JUST INCREMENT THE AMOUNT OF SICK PEOPLE AT THE SPOT;
+				tw_event_send(e); 
+			}
+			else if(new_recovery){
+				e = tw_event_new(prev_location_id, 1, lp);
+				m = tw_event_data(e); 
+				m->type = NEW_INFECTION //JUST DECREASE THE AMOUNT OF SICK PEOPLE AT THE SPOT;
+				tw_event_send(e); 
+			}
+			return;
+		}
+		tw_event *e_leave;
+		Msg_Data *m_leave;
+
+		tw_event *e_arrive;
+		Msg_Data *m_arrive;
+
+		tw_lpid new_location_id = get_location_id(x_spot, y_spot);
+
+
+		e_leave = tw_event_new(prev_location_id, 1, lp);
+		m_leave = tw_event_data(e_leave); 
+		m_leave->type = DEPARTURE //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & REMOVE THE PERSON FROM THE ARRAY OF PEOPLE IN THE SPOT;
+		m_leave->person_infected = previously_infected;
+		m_leave->person_id = lp->gid;
+		tw_event_send(e_leave); 
+
+		e_arrive = tw_event_new(new_location_id, 1, lp);
+		m_arrive = tw_event_data(e_arrive); 
+		m_arrive->type = ARRIVAL //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & ADD THE PERSON TO THE ARRAY OF PEOPLE IN THE SPOT;
+		m_arrive->person_infected = s->infected;
+		m_arrive->person_id = lp->gid;
+		tw_event_send(e_arrive); 
+	}
+}
+
+void 
+
 
 
 tw_lptype my_lps[] = {
@@ -67,16 +197,16 @@ tw_lptype my_lps[] = {
         .revent = person_event_reverse,
         .final = person_final,
         .map = NULL,
-        .state_sz = sizeof(Person),
+        .state_sz = sizeof(person),
     },
     {
-        .init = grid_init,
+        .init = location_int,
         .pre_run = NULL,
-        .event = grid_event,
-        .revent = grid_event_reverse,
-        .final = grid_final,
+        .event = location_event,
+        .revent = location_event_reverse,
+        .final = location_final,
         .map = NULL,
-        .state_sz = sizeof(Grid),
+        .state_sz = sizeof(location),
     },
     { 0 }, // End marker
 };
@@ -123,7 +253,7 @@ event_handler(person * s, tw_bf * bf, Msg_Data * msg, tw_lp * lp)
 	// Schedule a landing in the future
 	msg->saved_furthest_flight_landing = s->furthest_flight_landing;
 	s->furthest_flight_landing = ROSS_MAX(s->furthest_flight_landing, tw_now(lp));
-	ts = tw_rand_exponential(lp->rng, MEAN_LAND);
+	ts = c(lp->rng, MEAN_LAND);
 	e = tw_event_new(lp->gid, ts + s->furthest_flight_landing - tw_now(lp), lp);
 	m = tw_event_data(e);
 	m->type = LAND;
--- Starter Code/plagueInc.h
+++ Starter Code/plagueInc.h
@@ -4,12 +4,18 @@
 #include <ross.h>
 
 #define MOVES 30
-#define INFECTION_TIME 5
+#define INFECTION_TIME 50.0
+#define IMMUNE_TIME 100
 #define GRID_WIDTH 10
 #define GRID_HEIGHT 10
+#define STARTING_INFECTED_PERCENTAGE .1
+#define TRANSMISSION_COEFFICIENT .2
+#define LETHALITY .00001
+#define TRAVEL_LIKELIHOOD .8
+#define AVERAGE_MOVE_TIME 10.0
 
 //Move causes status check, after status check is done move again
-enum events { MOVE, STATUS_CHECK };
+enum events { ARRIVAL, DEPARTURE, NEW_INFECTION, NEW_RECOVERY, STATUS_CHECK};
 enum abs_directions { NORTH = 0, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST, NO_MOVE};
 
 
@@ -18,22 +24,29 @@ typedef struct {
 	int x_spot;
 	int y_spot;
 	int infected_time;
-    int immune_time;
+    bool infected;
+    bool susceptible;
 	bool alive;
+    bool immune;
 	enum abs_directions previous_move;
+
+    tw_stime infection_start;
+    tw_stime immune_start;
 } person;
 
 typedef struct {
 	int x_spot;
 	int y_spot;
-	int people_held;
-
-	//Maybe an array which holds the people in the spot
-} grid_spot;
+    int infected_count;
+    int total_people;
+	tw_lpid* people_held;
+} location;
 
 typedef struct {
 	enum events event_type;
-	Person person;
+    bool person_infected;
+    tw_lpid person_id;
+    int infected_count;
 } Msg_Data;
 
 #endif
