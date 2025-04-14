859731dc8d0d83b51833f83de10a60f5d24003b2
ChrisBleck <cbleck0808@gmail.com>
2025-04-14 15:06:14 -0400

    Add Location LP

--- Starter Code/plagueInc.c
+++ Starter Code/plagueInc.c
@@ -1,9 +1,30 @@
 #include "plagueInc.h"
 
+
+tw_lpid lp_type_mapper(tw_lpid gid) {
+    if (gid < NUM_LOCATIONS)
+        return 0; // LP type 0 = Location
+    else
+        return 1; // LP type 1 = Person
+}
+
+
+int get_x_spot(tw_lpid gid){
+	return gid % GRID_WIDTH;
+}
+
+int get_y_spot(tw_lpid gid){
+	int offset = get_x_spot(gid) * GRID_WIDTH;
+	return gid - offset;
+}
+
+tw_lpid get_location_id(int x_spot, int y_spot){
+	return x_spot * GRID_WIDTH + y_spot;
+}
+
 void
 person_init(person * s, tw_lp * lp)
 {
-  int i;
   tw_event *e;
   Msg_Data *m;
   s->moves_left = 30;
@@ -12,7 +33,7 @@ person_init(person * s, tw_lp * lp)
   s->alive = true;
 
   double r = tw_rand_unif(lp->rng);
-  if (r < TRANSMISSION_COEFFICIENT) {
+  if (r < STARTING_INFECTED_PERCENTAGE) {
 	s->infected_time = tw_now(lp);
 	s->susceptible = false;
 	s->infected = true;
@@ -21,12 +42,34 @@ person_init(person * s, tw_lp * lp)
 	s->infected = false;
   }
 
-
   e = tw_event_new(lp->gid, tw_rand_exponential(lp->rng, AVERAGE_MOVE_TIME), lp); //Schedules event at some future point in time
   m = tw_event_data(e); //retrieves the data pointer for the message associated with scheduled event, can modify message from here
   m->type = STATUS_CHECK;
   m->infected_count = 0;
   tw_event_send(e); //schedules event in ROSS
+
+  //TELL INITIAL LOCATION THAT PERSON HAS "ARRIVED" -> SPAWN PEOPLE
+  tw_lpid location_id = get_location_id(s->x_spot, s->y_spot);
+  tw_event *e_arrival;
+  Msg_Data *m_arrival;
+
+  e_arrive = tw_event_new(location_id, 1, lp);
+  m_arrive->type = ARRIVAL;
+  m_arrive->person_infected = s->infected;
+  m_arrive->person_id = lp->gid;
+  tw_event_send(e_arrive); 
+}
+
+void location_init(location * s, tw_lp * lp){
+  tw_event *e;
+  Msg_Data *m;
+  tw_lpid gid = lp->gid;
+  s->x_spot = get_x_spot(gid);
+  s->y_spot = get_y_spot(gid);
+  s->infected_count = 0;
+  s->total_people = 0;
+  s->max_people_held = INITIAL_MAX_PEOPLE_PER_SQUARE;
+  s->people_held = (tw_lpid *) malloc(sizeof(tw_lpid) * INITIAL_MAX_PEOPLE_PER_SQUARE);
 }
 
 
@@ -60,11 +103,6 @@ enum abs_directions assign_move(tw_lp * lp, int x_spot, int y_spot) {
     return possible_moves[tw_rand_integer(lp->rng, 0, count - 1)];
 }
 
-
-
-tw_lpid get_location_id(int x_spot, int y_spot){
-	//TODO: FILL IN
-}
 
 void
 person_event(person * s, tw_bf * bf, Msg_Data * msg, tw_lp * lp) {
@@ -171,21 +209,72 @@ person_event(person * s, tw_bf * bf, Msg_Data * msg, tw_lp * lp) {
 
 		e_leave = tw_event_new(prev_location_id, 1, lp);
 		m_leave = tw_event_data(e_leave); 
-		m_leave->type = DEPARTURE //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & REMOVE THE PERSON FROM THE ARRAY OF PEOPLE IN THE SPOT;
+		m_leave->type = DEPARTURE; //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & REMOVE THE PERSON FROM THE ARRAY OF PEOPLE IN THE SPOT
 		m_leave->person_infected = previously_infected;
 		m_leave->person_id = lp->gid;
 		tw_event_send(e_leave); 
 
 		e_arrive = tw_event_new(new_location_id, 1, lp);
 		m_arrive = tw_event_data(e_arrive); 
-		m_arrive->type = ARRIVAL //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & ADD THE PERSON TO THE ARRAY OF PEOPLE IN THE SPOT;
+		m_arrive->type = ARRIVAL; //ADJUST THE AMOUNT OF SICK PEOPLE AND TOTAL PEOPLE AT THE SPOT & ADD THE PERSON TO THE ARRAY OF PEOPLE IN THE SPOT
 		m_arrive->person_infected = s->infected;
 		m_arrive->person_id = lp->gid;
 		tw_event_send(e_arrive); 
 	}
 }
 
-void 
+void
+location_event(location * s, tw_bf * bf, Msg_Data * msg, tw_lp * lp)  {
+	//ARRIVAL, DEPARTURE, NEW_INFECTION, NEW_RECOVERY
+	enum events event = msg->event_type;
+	tw_lpid person_id = msg->person_id;
+	switch (event)
+	{
+	case ARRIVAL:
+		bool is_infected = msg->person_infected;
+		//If array is at max capacity and needs to be resized
+		int max_people_held = s->max_people_held;
+		if (s->total_people == max_people_held) {
+			s->max_people_held *= 2;
+			s->people_held = (tw_lpid *) realloc(s->people_held, sizeof(tw_lpid) * s->max_people_held);
+		}
+
+		if(is_infected){
+			s->infected_count++;
+		}
+		s->people_held[s->total_people] = person_id;
+		s->total_people++;
+		break;
+	case DEPARTURE:
+		bool was_infected = msg->person_infected;
+		for (int i = 0; i < s->total_people; i++){
+			if(s->people_held[i] == person_id){
+				s->people_held[i] == s->people_held[s->total_people-1];
+			}
+		}
+		if(is_infected){
+			s->infected_count--;
+		}
+		s->total_people--;
+		break;
+
+	case NEW_INFECTION:
+		s->infected_count++;
+		return;
+	case NEW_RECOVERY:
+		s->infected_count--;
+		return;
+	}
+
+	
+	tw_event *e;
+	Msg_Data *m;
+	e = tw_event_new(person_id, tw_rand_exponential(lp->rng, AVERAGE_MOVE_TIME), lp);
+	m = tw_event_data(e);
+	m->type = STATUS_CHECK;
+	m->infected_count = s->infected_count;
+	tw_event_send(e);
+}
 
 
 
@@ -200,7 +289,7 @@ tw_lptype my_lps[] = {
         .state_sz = sizeof(person),
     },
     {
-        .init = location_int,
+        .init = location_init,
         .pre_run = NULL,
         .event = location_event,
         .revent = location_event_reverse,
@@ -214,9 +303,7 @@ tw_lptype my_lps[] = {
 
 main(int argc, char **argv, char **env)
 {
-
-
 	tw_define_lps(nlp, sizeof(Msg_Data), 0);
 	g_tw_lp_types = my_lps;
-
+	g_tw_lp_typemap = lp_type_mapper;
 }
